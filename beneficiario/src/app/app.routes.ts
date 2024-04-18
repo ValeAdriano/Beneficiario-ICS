4381506a8b2990dd8732c76bfcde7b81a2d69587
ValeAdriano <acsvalefilho@gmail.com>
2024-04-18 15:22:32 -0300

    .

--- beneficiario/src/app/app.routes.ts
+++ beneficiario/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import path from 'path';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent}
+    
+];
