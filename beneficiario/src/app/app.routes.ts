cfad438962eeec90ceea2b887df18cb3dcfc4c46
ValeAdriano <acsvalefilho@gmail.com>
2024-04-22 16:07:17 -0300

    .

--- beneficiario/src/app/app.routes.ts
+++ beneficiario/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { LoginComponent } from './pages/login/login.component';
 import { MenuComponent } from './layout/menu/menu.component';
 import { AvisosComponent } from './pages/avisos/avisos.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
 
 
 export const routes: Routes = [
@@ -11,6 +12,7 @@ export const routes: Routes = [
     {path: 'avisos', component: AvisosComponent},
     {path: 'menu', component: MenuComponent},
     {path: 'home', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'perfil', component: PerfilComponent}
     
 ];
--- beneficiario/src/app/pages/perfil/perfil.component.ts
+++ beneficiario/src/app/pages/perfil/perfil.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../../layout/menu/menu.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-perfil',
     standalone: true,
     templateUrl: './perfil.component.html',
     styleUrl: './perfil.component.scss',
-    imports: [MenuComponent]
+    imports: [MenuComponent, RouterLink]
 })
 export class PerfilComponent {
 
